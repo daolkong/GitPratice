b024239a1ae22d64d8ecd6b5cc88c34a235778c3
daolkong <daolkong06@gmail.com>
2023-05-10 18:47:21 +0900

    cha

--- GitPratice/ContentView.swift
+++ GitPratice/ContentView.swift
@@ -8,18 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    let manysmile = ["😃","😄","😁","😆","🤣"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("daolkongzaban")
-            
+        
+        HStack {
+            ForEach(manysmile,
+                    id: \.self) { smile in
+                Text(smile)
+            }
         }
-        .padding()
+        
     }
 }
 
