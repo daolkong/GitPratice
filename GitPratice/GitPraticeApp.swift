6427f4f789ded2d21e404c45eefada50864bc04a
daolkong <daolkong06@gmail.com>
2023-05-10 10:25:35 +0900

    Initial Commit

---
+++ GitPratice/GitPraticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPraticeApp.swift
+//  GitPratice
+//
+//  Created by Daol on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct GitPraticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
